4824c0c1a5098627704b38e3eac332a6e383cd09
Stan Borbat <stan@borbat.com>
2013-11-06 19:00:15 -0500

    Added functionality to set owner and permissions on wordpress files

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,6 +4,8 @@ class wordpress::params {
     $owner = 'www-data'
     $group = 'www-data'
   } else {
+    $owner = 'www-data'
+    $group = 'www-data'
     fail("Class['wordpress']: Unsupported osfamily: ${::osfamily}")
   }
 
